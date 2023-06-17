472ac4d605907395a8ce2f243f8c98f14c84b0eb
Dhruv Pasricha <dhruvpasricha0002@gmail.com>
2023-06-17 12:47:47 +0530

    Add `Users` Seeder

---
+++ src/seeders/20230617063511-create-users.js
@@ -0,0 +1,30 @@
+"use strict";
+const faker = require("faker");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const users = [];
+
+        // Generate 10 random users
+        for (let i = 0; i < 10; i++) {
+            const user = {
+                firstName: faker.name.findName(),
+                lastName: faker.name.findName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+            users.push(user);
+        }
+
+        // Insert the generated users into the 'users' table
+        await queryInterface.bulkInsert("users", users, {});
+    },
+
+    async down(queryInterface, Sequelize) {
+        // Remove the inserted users from the 'users' table
+        await queryInterface.bulkDelete("users", null, {});
+    },
+};
